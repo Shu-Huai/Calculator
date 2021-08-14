12d7393176303717842a0bea4b969d12e559e350
殊怀丶 <lvzhihe_123@qq.com>
2021-08-14 22:42:02 +0800

    添加项目文件。

---
+++ Calculator/MainForm.Designer.vb
@@ -0,0 +1,280 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class mainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(mainForm))
+        Me.equalButton = New System.Windows.Forms.Button()
+        Me.plusButton = New System.Windows.Forms.Button()
+        Me.minusButton = New System.Windows.Forms.Button()
+        Me.dotButton = New System.Windows.Forms.Button()
+        Me.nineButton = New System.Windows.Forms.Button()
+        Me.timesButton = New System.Windows.Forms.Button()
+        Me.fiveButton = New System.Windows.Forms.Button()
+        Me.eightButton = New System.Windows.Forms.Button()
+        Me.devideButton = New System.Windows.Forms.Button()
+        Me.clearButton = New System.Windows.Forms.Button()
+        Me.sevenButton = New System.Windows.Forms.Button()
+        Me.fourButton = New System.Windows.Forms.Button()
+        Me.threeButton = New System.Windows.Forms.Button()
+        Me.zeroButton = New System.Windows.Forms.Button()
+        Me.sixButton = New System.Windows.Forms.Button()
+        Me.twoButton = New System.Windows.Forms.Button()
+        Me.oneButton = New System.Windows.Forms.Button()
+        Me.numberEditor = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'equalButton
+        '
+        Me.equalButton.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.equalButton.Location = New System.Drawing.Point(213, 255)
+        Me.equalButton.Name = "equalButton"
+        Me.equalButton.Size = New System.Drawing.Size(61, 94)
+        Me.equalButton.TabIndex = 53
+        Me.equalButton.Text = "="
+        Me.equalButton.UseVisualStyleBackColor = True
+        '
+        'plusButton
+        '
+        Me.plusButton.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.plusButton.Location = New System.Drawing.Point(213, 155)
+        Me.plusButton.Name = "plusButton"
+        Me.plusButton.Size = New System.Drawing.Size(61, 94)
+        Me.plusButton.TabIndex = 52
+        Me.plusButton.Text = "+"
+        Me.plusButton.UseVisualStyleBackColor = True
+        '
+        'minusButton
+        '
+        Me.minusButton.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.minusButton.Location = New System.Drawing.Point(213, 105)
+        Me.minusButton.Name = "minusButton"
+        Me.minusButton.Size = New System.Drawing.Size(61, 44)
+        Me.minusButton.TabIndex = 51
+        Me.minusButton.Text = "﹣"
+        Me.minusButton.UseVisualStyleBackColor = True
+        '
+        'dotButton
+        '
+        Me.dotButton.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.dotButton.Location = New System.Drawing.Point(146, 305)
+        Me.dotButton.Name = "dotButton"
+        Me.dotButton.Size = New System.Drawing.Size(61, 44)
+        Me.dotButton.TabIndex = 50
+        Me.dotButton.Text = "."
+        Me.dotButton.UseVisualStyleBackColor = True
+        '
+        'nineButton
+        '
+        Me.nineButton.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.nineButton.Location = New System.Drawing.Point(146, 155)
+        Me.nineButton.Name = "nineButton"
+        Me.nineButton.Size = New System.Drawing.Size(61, 44)
+        Me.nineButton.TabIndex = 49
+        Me.nineButton.Text = "9"
+        Me.nineButton.UseVisualStyleBackColor = True
+        '
+        'timesButton
+        '
+        Me.timesButton.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.timesButton.Location = New System.Drawing.Point(146, 105)
+        Me.timesButton.Name = "timesButton"
+        Me.timesButton.Size = New System.Drawing.Size(61, 44)
+        Me.timesButton.TabIndex = 48
+        Me.timesButton.Text = "×"
+        Me.timesButton.UseVisualStyleBackColor = True
+        '
+        'fiveButton
+        '
+        Me.fiveButton.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.fiveButton.Location = New System.Drawing.Point(79, 205)
+        Me.fiveButton.Name = "fiveButton"
+        Me.fiveButton.Size = New System.Drawing.Size(61, 44)
+        Me.fiveButton.TabIndex = 47
+        Me.fiveButton.Text = "5"
+        Me.fiveButton.UseVisualStyleBackColor = True
+        '
+        'eightButton
+        '
+        Me.eightButton.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.eightButton.Location = New System.Drawing.Point(79, 155)
+        Me.eightButton.Name = "eightButton"
+        Me.eightButton.Size = New System.Drawing.Size(61, 44)
+        Me.eightButton.TabIndex = 46
+        Me.eightButton.Text = "8"
+        Me.eightButton.UseVisualStyleBackColor = True
+        '
+        'devideButton
+        '
+        Me.devideButton.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.devideButton.Location = New System.Drawing.Point(79, 105)
+        Me.devideButton.Name = "devideButton"
+        Me.devideButton.Size = New System.Drawing.Size(61, 44)
+        Me.devideButton.TabIndex = 45
+        Me.devideButton.Text = "÷"
+        Me.devideButton.UseVisualStyleBackColor = True
+        '
+        'clearButton
+        '
+        Me.clearButton.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.clearButton.Location = New System.Drawing.Point(12, 105)
+        Me.clearButton.Name = "clearButton"
+        Me.clearButton.Size = New System.Drawing.Size(61, 44)
+        Me.clearButton.TabIndex = 44
+        Me.clearButton.Text = "C"
+        Me.clearButton.UseVisualStyleBackColor = True
+        '
+        'sevenButton
+        '
+        Me.sevenButton.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.sevenButton.Location = New System.Drawing.Point(12, 155)
+        Me.sevenButton.Name = "sevenButton"
+        Me.sevenButton.Size = New System.Drawing.Size(61, 44)
+        Me.sevenButton.TabIndex = 43
+        Me.sevenButton.Text = "7"
+        Me.sevenButton.UseVisualStyleBackColor = True
+        '
+        'fourButton
+        '
+        Me.fourButton.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.fourButton.Location = New System.Drawing.Point(12, 205)
+        Me.fourButton.Name = "fourButton"
+        Me.fourButton.Size = New System.Drawing.Size(61, 44)
+        Me.fourButton.TabIndex = 42
+        Me.fourButton.Text = "4"
+        Me.fourButton.UseVisualStyleBackColor = True
+        '
+        'threeButton
+        '
+        Me.threeButton.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.threeButton.Location = New System.Drawing.Point(146, 255)
+        Me.threeButton.Name = "threeButton"
+        Me.threeButton.Size = New System.Drawing.Size(61, 44)
+        Me.threeButton.TabIndex = 41
+        Me.threeButton.Text = "3"
+        Me.threeButton.UseVisualStyleBackColor = True
+        '
+        'zeroButton
+        '
+        Me.zeroButton.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.zeroButton.Location = New System.Drawing.Point(12, 305)
+        Me.zeroButton.Name = "zeroButton"
+        Me.zeroButton.Size = New System.Drawing.Size(128, 44)
+        Me.zeroButton.TabIndex = 40
+        Me.zeroButton.Text = "0"
+        Me.zeroButton.UseVisualStyleBackColor = True
+        '
+        'sixButton
+        '
+        Me.sixButton.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.sixButton.Location = New System.Drawing.Point(146, 205)
+        Me.sixButton.Name = "sixButton"
+        Me.sixButton.Size = New System.Drawing.Size(61, 44)
+        Me.sixButton.TabIndex = 39
+        Me.sixButton.Text = "6"
+        Me.sixButton.UseVisualStyleBackColor = True
+        '
+        'twoButton
+        '
+        Me.twoButton.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.twoButton.Location = New System.Drawing.Point(79, 255)
+        Me.twoButton.Name = "twoButton"
+        Me.twoButton.Size = New System.Drawing.Size(61, 44)
+        Me.twoButton.TabIndex = 38
+        Me.twoButton.Text = "2"
+        Me.twoButton.UseVisualStyleBackColor = True
+        '
+        'oneButton
+        '
+        Me.oneButton.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.oneButton.Location = New System.Drawing.Point(12, 255)
+        Me.oneButton.Name = "oneButton"
+        Me.oneButton.Size = New System.Drawing.Size(61, 44)
+        Me.oneButton.TabIndex = 37
+        Me.oneButton.Text = "1"
+        Me.oneButton.UseVisualStyleBackColor = True
+        '
+        'numberEditor
+        '
+        Me.numberEditor.Font = New System.Drawing.Font("Times New Roman", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.numberEditor.Location = New System.Drawing.Point(12, 12)
+        Me.numberEditor.Name = "numberEditor"
+        Me.numberEditor.ReadOnly = True
+        Me.numberEditor.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.numberEditor.Size = New System.Drawing.Size(262, 63)
+        Me.numberEditor.TabIndex = 36
+        Me.numberEditor.Text = "0"
+        Me.numberEditor.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        Me.numberEditor.WordWrap = False
+        '
+        'mainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(286, 360)
+        Me.Controls.Add(Me.equalButton)
+        Me.Controls.Add(Me.plusButton)
+        Me.Controls.Add(Me.minusButton)
+        Me.Controls.Add(Me.dotButton)
+        Me.Controls.Add(Me.nineButton)
+        Me.Controls.Add(Me.timesButton)
+        Me.Controls.Add(Me.fiveButton)
+        Me.Controls.Add(Me.eightButton)
+        Me.Controls.Add(Me.devideButton)
+        Me.Controls.Add(Me.clearButton)
+        Me.Controls.Add(Me.sevenButton)
+        Me.Controls.Add(Me.fourButton)
+        Me.Controls.Add(Me.threeButton)
+        Me.Controls.Add(Me.zeroButton)
+        Me.Controls.Add(Me.sixButton)
+        Me.Controls.Add(Me.twoButton)
+        Me.Controls.Add(Me.oneButton)
+        Me.Controls.Add(Me.numberEditor)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(302, 399)
+        Me.MinimumSize = New System.Drawing.Size(302, 399)
+        Me.Name = "mainForm"
+        Me.Text = "计算器"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents equalButton As Button
+    Friend WithEvents plusButton As Button
+    Friend WithEvents minusButton As Button
+    Friend WithEvents dotButton As Button
+    Friend WithEvents nineButton As Button
+    Friend WithEvents timesButton As Button
+    Friend WithEvents fiveButton As Button
+    Friend WithEvents eightButton As Button
+    Friend WithEvents devideButton As Button
+    Friend WithEvents clearButton As Button
+    Friend WithEvents sevenButton As Button
+    Friend WithEvents fourButton As Button
+    Friend WithEvents threeButton As Button
+    Friend WithEvents zeroButton As Button
+    Friend WithEvents sixButton As Button
+    Friend WithEvents twoButton As Button
+    Friend WithEvents oneButton As Button
+    Friend WithEvents numberEditor As TextBox
+End Class
---
+++ Calculator/MainForm.vb
@@ -0,0 +1,189 @@
+﻿Public Class mainForm
+    Private firstNumber_ As Double
+    Private secondNumber_ As Double
+    Private needRefresh_ As Boolean
+    Enum Operation
+        Plus
+        Minus
+        Times
+        Divide
+    End Enum
+    Private needOperation As Operation
+    Private Sub PushZero(sender As Object, e As EventArgs) Handles zeroButton.Click
+        If needRefresh_ Then
+            numberEditor.Text = "0"
+        Else
+            If Not numberEditor.Text = "0" Then
+                numberEditor.Text += "0"
+            End If
+        End If
+    End Sub
+    Private Sub PushOne(sender As Object, e As EventArgs) Handles oneButton.Click
+        If needRefresh_ Then
+            numberEditor.Text = "1"
+            needRefresh_ = False
+        Else
+            numberEditor.Text += "1"
+        End If
+    End Sub
+    Private Sub PushTwo(sender As Object, e As EventArgs) Handles twoButton.Click
+        If needRefresh_ Then
+            numberEditor.Text = "2"
+            needRefresh_ = False
+        Else
+            numberEditor.Text += "2"
+        End If
+    End Sub
+    Private Sub PushThree(sender As Object, e As EventArgs) Handles threeButton.Click
+        If needRefresh_ Then
+            numberEditor.Text = "3"
+            needRefresh_ = False
+        Else
+            numberEditor.Text += "3"
+        End If
+    End Sub
+    Private Sub PushFour(sender As Object, e As EventArgs) Handles fourButton.Click
+        If needRefresh_ Then
+            numberEditor.Text = "4"
+            needRefresh_ = False
+        Else
+            numberEditor.Text += "4"
+        End If
+    End Sub
+    Private Sub PushFive(sender As Object, e As EventArgs) Handles fiveButton.Click
+        If needRefresh_ Then
+            numberEditor.Text = "5"
+            needRefresh_ = False
+        Else
+            numberEditor.Text += "5"
+        End If
+    End Sub
+    Private Sub PushSix(sender As Object, e As EventArgs) Handles sixButton.Click
+        If needRefresh_ Then
+            numberEditor.Text = "6"
+            needRefresh_ = False
+        Else
+            numberEditor.Text += "6"
+        End If
+    End Sub
+    Private Sub PushSeven(sender As Object, e As EventArgs) Handles sevenButton.Click
+        If needRefresh_ Then
+            numberEditor.Text = "7"
+            needRefresh_ = False
+        Else
+            numberEditor.Text += "7"
+        End If
+    End Sub
+    Private Sub PushEight(sender As Object, e As EventArgs) Handles eightButton.Click
+        If needRefresh_ Then
+            numberEditor.Text = "8"
+            needRefresh_ = False
+        Else
+            numberEditor.Text += "8"
+        End If
+    End Sub
+    Private Sub PushNine(sender As Object, e As EventArgs) Handles nineButton.Click
+        If needRefresh_ Then
+            numberEditor.Text = "9"
+            needRefresh_ = False
+        Else
+            numberEditor.Text += "9"
+        End If
+    End Sub
+    Private Sub PushClear(sender As Object, e As EventArgs) Handles clearButton.Click
+        numberEditor.Text = "0"
+        needRefresh_ = True
+    End Sub
+    Private Sub PushPlus(sender As Object, e As EventArgs) Handles plusButton.Click
+        If firstNumber_ = Double.MinValue Then
+            firstNumber_ = numberEditor.Text
+            needRefresh_ = True
+        Else
+            firstNumber_ += numberEditor.Text
+            numberEditor.Text = firstNumber_
+            needRefresh_ = True
+        End If
+        needOperation = Operation.Plus
+    End Sub
+    Private Sub PushMinus(sender As Object, e As EventArgs) Handles minusButton.Click
+        If needRefresh_ Then
+            If firstNumber_ = Double.MinValue Then
+                firstNumber_ = numberEditor.Text
+                needRefresh_ = True
+                needOperation = Operation.Minus
+            Else
+                numberEditor.Text = "-"
+                needRefresh_ = False
+            End If
+
+        Else
+            If firstNumber_ = Double.MinValue Then
+                firstNumber_ = numberEditor.Text
+                needRefresh_ = True
+            Else
+                firstNumber_ -= numberEditor.Text
+                numberEditor.Text = firstNumber_
+                needRefresh_ = True
+            End If
+            needOperation = Operation.Minus
+        End If
+
+    End Sub
+    Private Sub PushTimes(sender As Object, e As EventArgs) Handles timesButton.Click
+        If firstNumber_ = Double.MinValue Then
+            firstNumber_ = numberEditor.Text
+            needRefresh_ = True
+        Else
+            firstNumber_ *= numberEditor.Text
+            numberEditor.Text = firstNumber_
+            needRefresh_ = True
+        End If
+        needOperation = Operation.Times
+    End Sub
+    Private Sub PushDevode(sender As Object, e As EventArgs) Handles devideButton.Click
+        If firstNumber_ = Double.MinValue Then
+            firstNumber_ = numberEditor.Text
+            needRefresh_ = True
+        Else
+            firstNumber_ /= numberEditor.Text
+            numberEditor.Text = firstNumber_
+            needRefresh_ = True
+        End If
+        needOperation = Operation.Divide
+    End Sub
+    Private Sub PushResult(sender As Object, e As EventArgs) Handles equalButton.Click
+        secondNumber_ = numberEditor.Text
+        If firstNumber_ = Double.MinValue Then
+            Return
+        End If
+        Select Case needOperation
+            Case Operation.Plus
+                numberEditor.Text = firstNumber_ + secondNumber_
+            Case Operation.Minus
+                numberEditor.Text = firstNumber_ - secondNumber_
+            Case Operation.Times
+                numberEditor.Text = firstNumber_ * secondNumber_
+            Case Operation.Divide
+                numberEditor.Text = firstNumber_ / secondNumber_
+        End Select
+        firstNumber_ = Double.MinValue
+        secondNumber_ = Double.MinValue
+        needRefresh_ = True
+    End Sub
+    Private Sub LoadMainForm(sender As Object, e As EventArgs) Handles MyBase.Load
+        firstNumber_ = Double.MinValue
+        secondNumber_ = Double.MinValue
+        needRefresh_ = True
+        numberEditor.Select(numberEditor.TextLength, 0)
+    End Sub
+    Private Sub PushDot(sender As Object, e As EventArgs) Handles dotButton.Click
+        If needRefresh_ Then
+            numberEditor.Text = "0."
+            needRefresh_ = False
+        ElseIf numberEditor.Text = "-" Then
+            numberEditor.Text = "-0."
+        Else
+            numberEditor.Text += "."
+        End If
+    End Sub
+End Class
